054a3558c6a0d1f814ac2460a3ad86fa4a865b0e
David Cormack <david@sentrika.net>
2017-10-31 06:45:02 +0000

    Updates to upgrade script.

--- files/tpcast-upgrade.sh
+++ files/tpcast-upgrade.sh
@@ -1,72 +1,9 @@
 #!/bin/bash
 
-# ---------------------------------------------------------
-# |                                                       |
-# |                 TPCast Upgrade Script                 |
-# |                   Installation Guide                  |
-# |                                                       |
-# ---------------------------------------------------------
+# OpenTPCast upgrade script for Raspian Stretch and VirtualHere.
+# An installation guide on how to use this script can be found at https://github.com/OpenTPCast/Docs/blob/master/guides/UPGRADE.md
 
-# Overview:
-# This guide provides instructions on how to upgrade a TPCast to Raspbian Stretch with Kernel 4.9+ and VirtualHere, which replaces the stock TPCast Connection Assistant software with a VirtualHere based solution, and enables use of the on-board microphone and auxiliary USB port in the HTC Vive.
-
-# Preparation:
-# This upgrade is not reversible, so it is STRONGLY RECOMMENDED that you BACKUP the MicroSD card of your TPCast power box so that it can be restored in the event that something goes wrong during the upgrade, or you want to restore back to the original factory state.  This requires removing the plastic covering of the TPCast power box (remove the 4 screws from the battery facing side and pry the grated plastic side off), removing the MicroSD and backing it up using a MicroSD card reader and cloning software such as Win32DiskImager (https://sourceforge.net/projects/win32diskimager/).
-# Ensure that any TPCast software is NOT RUNNING on your local machine during the upgrade.
-# Ensure that your TPCast power box battery is FULLY CHARGED and POWERED ON before attempting the upgrade.  Depending on your connection speed, it may be nessecary to disconnect the USB/power cables from the TPCast power box during the upgrade to further extend battery life.
-# Download & install Squid (http://squid.diladele.com/ for Windows, check https://wiki.squid-cache.org/SquidFaq/BinaryPackages for other platforms) on your local machine.
-# Download & install Putty SSH client (http://www.putty.org/ for Windows, use ssh command in other platforms) on your local machine.
-# Download & install VirtualHere USB Client (https://virtualhere.com/usb_client_software).  Note that a TPCast-optimized licence key must be purchased to use with VirtualHere USB Server after the upgrade (https://www.virtualhere.com/tpcast_purchase).
-# If you are a TPCast PRE user, you must replace all references to [ 192.168.144. ] with [ 192.168.1. ] in any steps mentioned during this guide.
-# If you own a USB to Ethernet adapter, you may alternatively skip installation of Squid Proxy Server and plug in a USB to Ethernet adapter to the USB port of the TPCast power box, and connect it to your own internet-enabled router, which will greatly improve download speeds during the upgrade.
-
-# Installation:
-# Power up the TPCast power box by plugging in the battery and wait a few minutes.
-# Launch your SSH client and connect with the following details:
-# Hostname: 192.168.144.88 (CE) or http://192.168.1.88 (PRE)
-# Port: 22
-# Username: pi
-# Password: 1qaz2wsx3edc4rfv
-
-# In your SSH client, run the following command if you opted to install Squid Proxy Server:
-# sudo wget -e check_certificate=off -e use_proxy=yes -e https_proxy=https://$(echo $SSH_CONNECTION | awk '{print $1}'):3128 https://rawgit.com/OpenTPCast/Docs/master/files/tpcast-upgrade.sh && sudo chmod +x ./tpcast-upgrade.sh && sudo ./tpcast-upgrade.sh $(echo $SSH_CONNECTION | awk '{print $1}')
-
-# In your SSH client, run the following command if you opted to use a USB to Ethernet adapter in place of Squid Proxy Server:
-# sudo wget https://rawgit.com/OpenTPCast/Docs/master/files/tpcast-upgrade.sh && sudo chmod +x ./tpcast-upgrade.sh && sudo ./tpcast-upgrade.sh
-
-# After the upgrade has finished (in approximately 1-2 hours), launch VirtualHere USB Client on your local machine and wait a few minutes following the reboot notification.  If your VirtualHere client does not detect the TPCast after 5 minutes following the reboot, remove and reinsert the battery to the power box and wait a further 5 minutes.
-
-# Configuring VirtualHere for TPCast:
-# Purchase and apply your VirtualHere USB Server unlimited licence key in VirtualHere USB Client by selecting Licence, Enter Licence(s) and copy your licence key from the email received following purchase.
-# In VirtualHere USB Client, expand USB Hubs, expand TPCast, then right click and select "Auto-Use Device/Port" for each of the following devices:
-# - HTC Vive
-# - Lighthouse FPGA RX
-# - Watchman Dongle
-# - Watchman Dongle
-# - USB Audio Device
-# Right click on USB Hubs then select Install Client Service.
-
-# Launching TPCast:
-# To load up your TPCast on future play sessions, plug in the TPCast, wait a few minutes (checking VirtualHere USB Client if nessecary to see if the TPCast is ready), then launch SteamVR.
-
-# Clean Up:
-# Any TPCast software should be left disabled or uninstalled while using VirtualHere USB Client.
-# Squid and Putty are no longer needed once everything is confirmed as working correctly and can be safely uninstalled from your local machine.
-
-# Performance Optimizations:
-# If you experience regular tracking issues, it may be nessecary to update the stock TPCast router to operate in "11a only" network mode.
-# Connect to the router at http://192.168.144.1 (CE) or http://192.168.1.1 (PRE)
-# Username: tproot (if prompted)
-# Password: 8427531 (CE) or 12345678 (PRE)
-# Navigate to WLAN Settings > Basic Settings > 5G and change Network Mode from "11vht AC/AN/A" to "11a only".
-# Click Save to apply the changes.
-
-# If you continue to experience tracking issues, you may have to select a different Channel by navigating to WLAN Settings > Basic Settings > 5G, and change Channel from "AutoSelect" to a different option.  The channel you select will vary based on region, outside interference and network configuration so try each channel until one works well for your setup.
-
-# Alternatively, using a more reliable router to overcome poor connection or bandwidth issues exhibited by the stock TPCast router may be an option.  The following routers have been tested with the TPCast upgrade and have shown to provide substantial improvements to tracking reliability over the stock TPCast router:
-# - Asus RT-AC68U
-
-# ---------------------------------------------------------
+# Author: Genesis (https://github.com/NGenesis)
 
 # Stop on errors
 set -e
